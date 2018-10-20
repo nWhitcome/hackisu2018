7a422e8a31598e869280675b240d37d5ed473cf1
nWhitcome <whitcome@iastate.edu>
2018-10-20 08:00:10 -0500

    initial git push

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/index.html');
+});
+app.listen(8080);
+console.log('listening on port 8080')
