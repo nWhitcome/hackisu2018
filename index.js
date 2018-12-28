990fd335c3d1d64f325852348a54645fa26df5b7
Dave <contact@davidwhitcome.com>
2018-12-28 00:15:13 -0600

    require http and httpServer setup

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+var port = process.env.PORT || 3000;
+var http = require('http');
 var express = require('express');
 var app = express();
-var port_number = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
-app.listen(port_number);
-console.log('listening on port ' + port_number)
+
+var httpServer = http.createServer(app);
+httpServer.listen(port);
+console.log('listening on port ' + port)
